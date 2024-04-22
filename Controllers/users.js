0ca9867dc97c0f9bdc9e360b4a76fe1375562b43
aiaja <aisyafirst@gmail.com>
2024-04-23 00:46:57 +0700

    unggah lanjutan

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,25 +1,16 @@
-import express from "express";
 import { prisma } from "../Lib/prisma.js";
 
-export const users = express.Router()
+function postCreate(req,res) {
+    async(req,res) => {
+        await prisma.user.create({
+            data: req.body
+        })
+        res.sendStatus(201);
+    }
+}
 
-users.get('/', (req, res) => {
-    res.send({users:[
-        "joko",
-        "dono"
-    ]});
-});
-
-//create user
-users.post('/create', async(req,res) => {
-    await prisma.user.create({
-        data: req.body
-    })
-    res.sendStatus(201);
-})
-
-//read All user
-users.get('/readAll', async(req,res)=>{
+function getReadAll(req,res) {
+    async(req,res)=>{
     const limit = req.query.limit
     const page = req.query.page
 
@@ -27,45 +18,131 @@ users.get('/readAll', async(req,res)=>{
         take: limit && parseInt(limit),
         skip: page && parseInt(page)
     })
-
-
     res.json(users)
-})
-
-//read user
-users.get('/read/:id', async(req,res) => {
-    const id = parseInt(req.params.id)
-    const user = await prisma.users.findFirst({
-        where:{
-            id
-        }
-    })
-    if(!user){
-        res.status(404).json({
-            Error:true,
-            message:"user tidak ditemukan"
-        })
-        return
     }
-    res.json(user)
-})
+};
 
-//update user
-users.put('/update/:id', async(req,res) => {
-    const id = parseInt(req.params.id)
-    await prisma.users.update({
-        data:req.body,
-        where:{id}
-    })
+function getRead(req,res) {
+    async(req,res) => {
+        const id = parseInt(req.params.id)
+        const user = await prisma.users.findFirst({
+            where:{
+                id
+            }
+        })
+        if(!user){
+            res.status(404).json({
+                Error:true,
+                message:"user tidak ditemukan"
+            })
+            return
+        }
+        res.json(user)
+    }
+};
 
-    res.sendStatus(200)
-})
+function putUpdate(req,res) {
+    async(req,res) => {
+        const id = parseInt(req.params.id)
+        await prisma.users.update({
+            data:req.body,
+            where:{id}
+        })
+    
+        res.sendStatus(200)
+    }
+};
 
-users.delete('/delete/:id', async(req,res) => {
-    const id = parseInt(req.params.id)
-    await prisma.users.delete({
-        where:{id}
-    })
+function delDelete(req,res) {
+    async(req,res) => {
+        const id = parseInt(req.params.id)
+        await prisma.users.delete({
+            where:{id}
+        })
+    
+        res.sendStatus(200)
+    }
+};
 
-    res.sendStatus(200)
-})
+function postDef(req,res) {
+    console.log("user post : ", req.body)
+    res.sendStatus(201)
+}
+
+export {
+    postDef,
+    postCreate,
+    getRead,
+    getReadAll,
+    putUpdate,
+    delDelete,
+};
+
+// export const users = express.Router()
+
+// users.get('/', (req, res) => {
+//     res.send({users:[
+//         "joko",
+//         "dono"
+//     ]});
+// });
+
+// //create user
+// users.post('/create', async(req,res) => {
+//     await prisma.user.create({
+//         data: req.body
+//     })
+//     res.sendStatus(201);
+// })
+
+// //read All user
+// users.get('/readAll', async(req,res)=>{
+//     const limit = req.query.limit
+//     const page = req.query.page
+
+//     const users = await prisma.users.findMany({
+//         take: limit && parseInt(limit),
+//         skip: page && parseInt(page)
+//     })
+
+
+//     res.json(users)
+// })
+
+// //read user
+// users.get('/read/:id', async(req,res) => {
+//     const id = parseInt(req.params.id)
+//     const user = await prisma.users.findFirst({
+//         where:{
+//             id
+//         }
+//     })
+//     if(!user){
+//         res.status(404).json({
+//             Error:true,
+//             message:"user tidak ditemukan"
+//         })
+//         return
+//     }
+//     res.json(user)
+// })
+
+// //update user
+// users.put('/update/:id', async(req,res) => {
+//     const id = parseInt(req.params.id)
+//     await prisma.users.update({
+//         data:req.body,
+//         where:{id}
+//     })
+
+//     res.sendStatus(200)
+// })
+
+// users.delete('/delete/:id', async(req,res) => {
+//     const id = parseInt(req.params.id)
+//     await prisma.users.delete({
+//         where:{id}
+//     })
+
+//     res.sendStatus(200)
+// })
--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 import express from "express";
-import { users } from "./Controllers/users.js";
+import userRouter from "./Router/userRouter.js";
+import blogRouter from "./Router/blogRouter.js";
+import parser from "body-parser";
+import isActiveRoute from "./Helpers/routeHelpers.js";
+import session from "express-session";
 import { POST, authenticateToken} from "./Auth/login.js";
 import signup from './Auth/signup.js';
-import parser from "body-parser";
 
 const port = 3000;
 const app = express();
 
-app.use(parser.json())
-app.use('/users',users)
+app.use(parser.json());
+app.use(express.json());
 
-app.listen(port,()=>{
-    console.log('listening on port : ' + port);
-})
+// Mounting userRouter at '/users' base path
+app.use('/users', userRouter);
 
-users.post('/', (req,res) => {
-    console.log("user post : ", req.body)
-    res.sendStatus(201)
-})
+// Mounting blogRouter at '/blog' base path
+app.use('/blog', blogRouter);
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: "d283hej1io2kmsxni32r239uejsan",
+}))
 
 app.post('/signup', signup)
 app.post('/login', POST);
@@ -26,4 +32,17 @@ app.get('/protected', authenticateToken, (req,res) => {
 });
 app.post('/logout', authenticateToken, (req,res)=> {
     res.json({message: "Logout successfull"});
+})
+app.locals.isActiveRoute = isActiveRoute;
+
+const myMiddleware = (req, res, next) => {
+    console.log('This is a middleware function');
+    next(); // Call next to pass control to the next middleware in the stack
+  };
+  
+  // Register middleware globally
+app.use(myMiddleware);
+
+app.listen(port,()=>{
+    console.log('listening on port : ' + port);
 })
